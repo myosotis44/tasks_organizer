281ab929c23a5db5c6a6e965f1c0a6f7d39833bf
Victoria Gouin <victoria.gouin@gmail.com>
2023-12-08 17:02:11 +0100

    modeles + migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Modèles Collection et Task
+class Collection(models.Model):
+    name = models.CharField(max_length=60)
+    slug = models.SlugField()
+
+class Task(models.Model):
+    description = models.CharField(max_length=300)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
